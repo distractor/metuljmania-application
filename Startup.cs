7a42c1d2bc9e4d8b709f63d31162265a977c9218
mjancic <mitjajancic@gmail.com>
2021-08-03 19:14:29 +0200

    Removing https redirect

--- Startup.cs
+++ Startup.cs
@@ -98,8 +98,6 @@ namespace MetuljmaniaDatabase
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseHttpsRedirection();
-
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
